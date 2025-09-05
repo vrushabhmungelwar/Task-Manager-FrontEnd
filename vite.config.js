b69690a14f4b843502470a06dd0eeb06e4d7dd5e
Vrushabh Mungelwar <vrushabhmungelwar10@gmail.com>
2025-09-05 17:25:59 +0530

    v2

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,15 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   server: {
+    allowedHosts: [
+      "localhost",
+      "127.0.0.1",
+      "task-manager-frontend-4f0m.onrender.com",
+      "mycustomdomain.com",
+    ],
     host: "0.0.0.0",
     port: 5173,
     proxy: {
